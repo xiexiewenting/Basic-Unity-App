ef199c023c289835bd12abb9448ccc6ac9773962
Wenting Xie <wentingxie@kinuting-pro.local>
2019-09-30 01:34:06 -0400

    finished up to section 3.12

---
+++ Assets/Scripts/StartGoldberg.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StartGoldberg : MonoBehaviour
+{
+    //public GameObject _startPlatform;
+    public TextMeshProUGUI _textMeshPro;
+    public string _standByText, startedText;
+
+    private Camera _mainCamera;
+    private bool _rayDidHit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mainCamera = Camera.main;
+        _textMeshPro.text = _standByText;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetMouseButtonUp(0)) && (_rayDidHit == false)) 
+        {
+            //Debug.Log("Pressed left click.");
+            Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            _rayDidHit = Physics.Raycast(ray, out hitInfo);
+            if (_rayDidHit)
+            {
+                Interact(hitInfo);
+            }
+        }
+    }
+
+    void Interact(RaycastHit hit)
+    {
+        //Debug.Log("HITTING" + hit.collider.name);
+        if (hit.collider.name == gameObject.name)
+        {
+            gameObject.SetActive(false);
+            _textMeshPro.text = startedText;
+        }
+    }
+}
